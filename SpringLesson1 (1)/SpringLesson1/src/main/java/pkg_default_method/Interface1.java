594844dcc6f87b7af189abe8845355a52ee8f0e3
Minh Ha <minhha1192003@gmail.com>
2025-07-09 23:49:19 +0700

    default

--- SpringLesson1 (1)/SpringLesson1/src/main/java/pkg_default_method/Interface1.java
+++ SpringLesson1 (1)/SpringLesson1/src/main/java/pkg_default_method/Interface1.java
@@ -5,4 +5,30 @@ public interface Interface1 {
     {
         System.out.println("Interface,method1");
     }
+    public interface Inerface2
+    {
+        default void method2()
+        {
+            System.out.println("Inter2,method");
+        }
+    }
+}
+class MulInheristance implements Interface1, Interface1.Inerface2 {
+
+    @Override
+    public void method1()
+    {
+        Interface1.super.method1();
+    }
+    // trung voi phuong thuc method2 trong interface2
+    public void method2()
+    {
+        System.out.println("MulInheristance.Interfacw2");
+    }
+
+    public static void main(String[] args) {
+        MulInheristance mi = new MulInheristance();
+        mi.method1();
+        mi.method2();
+    }
 }
