ee1e5774d32f7adf3ec5f818c4796f40de01ce7e
Minh Ha <minhha1192003@gmail.com>
2025-07-11 23:52:09 +0700

    Main

--- DevmasterLesson2/DevmasterLesson2/src/main/java/DI/Main.java
+++ DevmasterLesson2/DevmasterLesson2/src/main/java/DI/Main.java
@@ -3,6 +3,11 @@ package DI;
 public class Main {
     public static void main(String[] args) {
         // khoi tao doi tuong phu thuoc ben ngoai (IOc)
-        
+        MessageService emailService= new EmailService();
+        NotificationClient client1 = new NotificationClient(emailService);
+        client1.notifyUser("Chao mung ban den vs Ioc bang email");
+        //Injrct dependency thong qua setter
+        NotificationClient client3 = new NotificationClient(null);
+        client3.notifyUser("Thong bao bang Email qua seter");
     }
 }
