4caf3a245a7dda5c3c1d87e89737c8cf6cbb0123
Minh Ha <minhha1192003@gmail.com>
2025-07-15 20:15:01 +0700

    Student_entity

---
+++ Devmaster_Leson3/Devmaster_Leson3/src/main/java/Devmaster_Leson3/Service/ServicesStudent.java
@@ -0,0 +1,13 @@
+package Devmaster_Leson3.Service;
+
+import Devmaster_Leson3.Entity.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ServicesStudent {
+    List<Student>students= new ArrayList<Student>();
+
+}
