f42dcae2ab31de6cc90532a56e1a9d58f3dcfa50
Minh Ha <minhha1192003@gmail.com>
2025-07-15 19:57:47 +0700

    Lesson3

---
+++ Devmaster_Leson3/Devmaster_Leson3/src/main/java/Devmaster_Leson3/DevmasterLeson3Application.java
@@ -0,0 +1,13 @@
+package Devmaster_Leson3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmasterLeson3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmasterLeson3Application.class, args);
+	}
+
+}
---
+++ Devmaster_Leson3/Devmaster_Leson3/src/test/java/Devmaster_Leson3/DevmasterLeson3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Devmaster_Leson3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmasterLeson3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
