0f1d14fd33bf009c47a340f8819580b672cdcdda
Minh Ha <minhha1192003@gmail.com>
2025-07-15 20:02:41 +0700

    xay dung mo hinh

---
+++ Devmaster_Leson3/Devmaster_Leson3/src/main/java/Devmaster_Leson3/Entity/Student.java
@@ -0,0 +1,11 @@
+package Devmaster_Leson3.Entity;
+
+public class Student {
+    long id;
+    String name;
+    int age;
+    String gender;
+    String address;
+    String phone;
+    String email;
+}
