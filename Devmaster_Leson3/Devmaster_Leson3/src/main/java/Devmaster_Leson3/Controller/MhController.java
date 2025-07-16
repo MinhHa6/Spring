b3ba92251ef6c6ccb1013c96da2e5652a1d09863
Minh Ha <minhha1192003@gmail.com>
2025-07-16 22:35:55 +0700

    MhController

---
+++ Devmaster_Leson3/Devmaster_Leson3/src/main/java/Devmaster_Leson3/Controller/MhController.java
@@ -0,0 +1,44 @@
+package Devmaster_Leson3.Controller;
+
+import Devmaster_Leson3.Entity.MonHoc;
+import Devmaster_Leson3.Entity.Student;
+import Devmaster_Leson3.Service.ServiceMH;
+import Devmaster_Leson3.Service.ServicesStudent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MhController {
+    @Autowired
+    private ServiceMH servicesMh;
+    @GetMapping("/Mh-list")
+    public List<MonHoc> getAllStudents()
+    {
+        return servicesMh.getMonHocs();
+    }
+    @GetMapping("/mh/{mamh}")
+    public MonHoc getAllMHs(@PathVariable String mamh)
+    {
+        String param=mamh;
+        return servicesMh.getMhmaMh(param);
+    }
+    @PostMapping("/mh-add")
+    public MonHoc addMh(@RequestBody MonHoc mh)
+    {
+        return servicesMh.addMh(mh);
+    }
+    @PutMapping("/mh/{mamh}")
+    public MonHoc updateMh(@PathVariable String mamh,@RequestBody MonHoc mh)
+    {
+        String param=mamh;
+        return servicesMh.updateMH(param,mh);
+    }
+    @DeleteMapping("/mh/{mamh}")
+    public boolean deleteMH(@PathVariable String mamh)
+    {
+        String param=mamh;
+        return servicesMh.deleteMH(param);
+    }
+}
