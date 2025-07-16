2cda59e4f3138477da996a42c81ff2b58e329704
Minh Ha <minhha1192003@gmail.com>
2025-07-16 22:01:57 +0700

    entity MH

---
+++ Devmaster_Leson3/Devmaster_Leson3/src/main/java/Devmaster_Leson3/Entity/MonHoc.java
@@ -0,0 +1,37 @@
+package Devmaster_Leson3.Entity;
+
+public class MonHoc {
+    String maMH;
+    String tenMH;
+    int soTiet;
+
+    public MonHoc(String maMH, String tenMH, int soTiet) {
+        this.maMH = maMH;
+        this.tenMH = tenMH;
+        this.soTiet = soTiet;
+    }
+
+    public String getMaMH() {
+        return maMH;
+    }
+
+    public void setMaMH(String maMH) {
+        this.maMH = maMH;
+    }
+
+    public String getTenMH() {
+        return tenMH;
+    }
+
+    public void setTenMH(String tenMH) {
+        this.tenMH = tenMH;
+    }
+
+    public int getSoTiet() {
+        return soTiet;
+    }
+
+    public void setSoTiet(int soTiet) {
+        this.soTiet = soTiet;
+    }
+}
