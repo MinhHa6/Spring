ff0ed09d725ad055db0ca081d2b9f9d20b17fd96
Minh Ha <minhha1192003@gmail.com>
2025-07-18 23:12:30 +0700

    DTO makh,TenKh

--- Devmaster_Lesson4/Devmaster_Lesson4/src/main/java/Devmaster_Lesson4/dto/KhoaDTO.java
+++ Devmaster_Lesson4/Devmaster_Lesson4/src/main/java/Devmaster_Lesson4/dto/KhoaDTO.java
@@ -1,4 +1,24 @@
 package Devmaster_Lesson4.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class KhoaDTO {
+    @NotBlank(message = "Ma khoa cannot blank")
+    @Size(min = 2,message = "Ma khoa toi thieu 2 ky tu")
+    String maKh;
+    @NotBlank(message = "Ma khoa cannot blank")
+    @Size(min = 2,message = "Ma khoa toi thieu 2 ky tu")
+    String tenKh;
+
 }
