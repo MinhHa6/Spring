3480323b0dc028e906e15dd9d8ac9569cb2ffefb
Minh Ha <minhha1192003@gmail.com>
2025-07-19 21:54:42 +0700

    Khoa service

--- Devmaster_Leson3/Devmaster_Leson3/src/main/java/Devmaster_Leson3/Service/ServiceKhoa.java
+++ Devmaster_Leson3/Devmaster_Leson3/src/main/java/Devmaster_Leson3/Service/ServiceKhoa.java
@@ -20,4 +20,37 @@ public class ServiceKhoa {
     public List<Khoa> getKhoas() {
         return khoas;
     }
+    // thay thong tin theo ma khoa
+    public Khoa getMaKh(String mk)
+    {
+        return khoas.stream().filter(kh -> kh.getMaKh().equals(mk)).findFirst().orElse(null);
+    }
+    //them khoa moi
+    public Khoa addKh(Khoa kh)
+    {
+        khoas.add(kh);
+        return kh;
+    }
+    // cap nhat thong tin ma khoa
+    public Khoa updateKh(String makh,Khoa kh)
+    {
+        Khoa check=getMaKh(makh);
+        if (check==null)
+        {
+            return null;
+        }
+        khoas.forEach(item->{
+            if (item.getMaKh()==makh){
+                item.setMaKh(kh.getMaKh());
+                item.setTenKh(kh.getTenKh());
+            }
+        });
+        return kh;
+    }
+    // xoa thong tin khoa
+    public Boolean deleteKh(String maKh)
+    {
+        Khoa par=getMaKh(maKh);
+        return khoas.remove(par);
+    }
 }
