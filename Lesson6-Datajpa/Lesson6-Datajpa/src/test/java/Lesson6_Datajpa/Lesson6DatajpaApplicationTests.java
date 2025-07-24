c7fdf03346d0f1177db5dafdef6ec5c681b87076
Minh Ha <minhha1192003@gmail.com>
2025-07-24 19:23:03 +0700

    Lesson6-Data

---
+++ Lesson6-Datajpa/Lesson6-Datajpa/src/test/java/Lesson6_Datajpa/Lesson6DatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package Lesson6_Datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson6DatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
