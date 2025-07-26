d7154669e00148f3fc485b874c583cb5fe6c7b8c
Minh Ha <minhha1192003@gmail.com>
2025-07-27 04:01:20 +0700

    fix chuaw xong

--- Lesson6-Datajpa/Lesson6-Datajpa/src/main/java/Lesson6_Datajpa/controller/CustomerController.java
+++ Lesson6-Datajpa/Lesson6-Datajpa/src/main/java/Lesson6_Datajpa/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package Lesson6_Datajpa.controller;
 
+import Lesson6_Datajpa.dto.CustomerDTO;
 import Lesson6_Datajpa.entity.Customer;
 import Lesson6_Datajpa.service.CustomerService;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.concurrent.CompletionService;
@@ -26,9 +29,16 @@ public class CustomerController {
         return  "students/customer-list";
     }
     @GetMapping("/add-new")
-    public String addNeCustomer(Model model)
+    public String addNewCustomer(Model model) {
+        model.addAttribute("customer", new CustomerDTO());
+        return "students/customer-add"; // ✅ Trùng với file bạn đã tạo
+    }
+
+
+    @PostMapping
+    public String saveCustomer(@ModelAttribute("customer")CustomerDTO customerDTO)
     {
-        model.addAttribute("customer",new Customer());
-        return "customers/customer-add";
+        customerService.save(customerDTO);
+        return "redirect:/customers";
     }
 }
