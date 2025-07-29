7cf6dd5a2d89308af6202ef7437f1ca3536fea16
Minh Ha <minhha1192003@gmail.com>
2025-07-29 19:03:03 +0700

    moi quan he trong sql

---
+++ Devmaster-Lesson7/Lesson7/src/main/java/Devmaster_Lesson7/demo/DevmasterLesson7Application.java
@@ -0,0 +1,13 @@
+package Devmaster_Lesson7.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmasterLesson7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmasterLesson7Application.class, args);
+	}
+
+}
---
+++ Devmaster-Lesson7/Lesson7/src/test/java/Devmaster_Lesson7/demo/DevmasterLesson7ApplicationTests.java
@@ -0,0 +1,13 @@
+package Devmaster_Lesson7.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmasterLesson7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
