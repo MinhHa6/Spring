caf709b78cdbdc7f640b5c33bffe193e093290c4
Minh Ha <minhha1192003@gmail.com>
2025-08-05 20:05:11 +0700

    many sang one

---
+++ Devmaster-Lesson09/Devmaster-Lesson09/src/main/java/Devmaster_Lesson09/DevmasterLesson09Application.java
@@ -0,0 +1,13 @@
+package Devmaster_Lesson09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmasterLesson09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmasterLesson09Application.class, args);
+	}
+
+}
---
+++ Devmaster-Lesson09/Devmaster-Lesson09/src/test/java/Devmaster_Lesson09/DevmasterLesson09ApplicationTests.java
@@ -0,0 +1,13 @@
+package Devmaster_Lesson09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmasterLesson09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
