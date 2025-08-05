a7d9b7491c7bf0376ea825157721db8017abd1ad
Minh Ha <minhha1192003@gmail.com>
2025-08-05 11:49:56 +0700

    Lesson 8 -many to many'

---
+++ Devmaster-Lesson8/Devmaster-Lesson8/src/test/java/Devmaster_Leson8/DevmasterLesson8ApplicationTests.java
@@ -0,0 +1,13 @@
+package Devmaster_Leson8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmasterLesson8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
