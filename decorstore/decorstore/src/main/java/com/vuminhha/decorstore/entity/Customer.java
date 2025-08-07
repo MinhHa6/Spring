cdae991218ccb78d281388711d02030df80c381c
Minh Ha <minhha1192003@gmail.com>
2025-08-07 21:13:43 +0700

    entity payment va customer

---
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/entity/Customer.java
@@ -0,0 +1,68 @@
+package com.vuminhha.decorstore.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Customer")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 250)
+    private String name;
+
+    @Column(length = 50)
+    private String username;
+
+    @Column(length = 32)
+    private String password;
+
+    @Column(length = 500)
+    private String address;
+
+    @Column(length = 150)
+    private String email;
+
+    @Column(length = 50)
+    private String phone;
+
+    @Column(length = 250)
+    private String avatar;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    private Long createdBy;
+
+    private Long updatedBy;
+
+    @Column(columnDefinition = "TINYINT", nullable = false)
+    private Boolean isDelete = false;
+
+    @Column(columnDefinition = "TINYINT", nullable = false)
+    private Boolean isActive = true;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+        this.updatedDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedDate = LocalDateTime.now();
+    }
+}
---
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/entity/PaymentMethod.java
@@ -0,0 +1,49 @@
+package com.vuminhha.decorstore.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Payment_Method")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentMethod {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 250)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String notes;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    @Column(columnDefinition = "TINYINT", nullable = false)
+    private Boolean isDelete = false;
+
+    @Column(columnDefinition = "TINYINT", nullable = false)
+    private Boolean isActive = true;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = LocalDateTime.now();
+        this.updatedDate = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedDate = LocalDateTime.now();
+    }
+}
