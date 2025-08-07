7a3faab020186dfaf2e7f005ee44ac6c0e6b0097
Minh Ha <minhha1192003@gmail.com>
2025-08-07 19:51:59 +0700

    project thuong mai dien tu

---
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/DecorstoreApplication.java
@@ -0,0 +1,13 @@
+package com.vuminhha.decorstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecorstoreApplication.class, args);
+	}
+
+}
