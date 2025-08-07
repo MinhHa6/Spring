1fd90f79e17e0307d462505b433d62b0f779f43f
Minh Ha <minhha1192003@gmail.com>
2025-08-07 20:10:57 +0700

    entity

---
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/entity/Category.java
@@ -0,0 +1,56 @@
+package com.vuminhha.decorstore.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@Table(name = "Category")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(length = 500)
+    private  String name;
+
+    @Column(columnDefinition = "Text")
+    private String notes;
+
+    @Column(length = 250)
+    private String icon;
+
+    private Long idParent;
+    @Column(columnDefinition = "TINYINT")
+    private Integer type;
+
+    @Column(length = 160)
+    private String slug;
+
+    @Column(length = 100)
+    private String metaTitle;
+
+    @Column(length = 300)
+    private String metaKeyword;
+
+    @Column(length = 300)
+    private String metaDescription;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    private Long createdBy;
+
+    private Long updatedBy;
+
+
+
+}
