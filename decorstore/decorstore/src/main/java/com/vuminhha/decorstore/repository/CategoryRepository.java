6b23729105c33597c3c0ebe9c52e5ddec76ef927
Minh Ha <minhha1192003@gmail.com>
2025-08-08 22:24:47 +0700

    categoryRepository

---
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.vuminhha.decorstore.repository;
+
+import com.vuminhha.decorstore.entity.Category;
+import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
