4f37fa646f7ad28d78dd266bcb09d0acf79741f5
Minh Ha <minhha1192003@gmail.com>
2025-08-11 23:33:58 +0700

    repository33

---
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/repository/TransportMethodRepository.java
@@ -0,0 +1,7 @@
+package com.vuminhha.decorstore.repository;
+
+import com.vuminhha.decorstore.entity.TransportMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransportMethodRepository extends JpaRepository<TransportMethod,Long> {
+}
