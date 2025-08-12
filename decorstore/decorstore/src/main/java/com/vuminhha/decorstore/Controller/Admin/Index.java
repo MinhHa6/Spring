8bb6808ee858e37c7f6fb26da0cb611c480b3feb
Minh Ha <minhha1192003@gmail.com>
2025-08-12 23:00:01 +0700

    admin/homeadmin

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/Admin/Index.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/Admin/Index.java
@@ -1,11 +1,13 @@
 package com.vuminhha.decorstore.Controller.Admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class Index {
+    @GetMapping
     public String admin()
     {
         return "admin/index";
--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/repository/NewCategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.vuminhha.decorstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewCategoryRepository extends JpaRepository<NewCategoryRepository,Long> {
-}
