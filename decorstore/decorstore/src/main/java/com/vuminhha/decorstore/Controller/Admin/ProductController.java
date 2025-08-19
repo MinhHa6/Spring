a0b0aa99603b9c9303d44a491a167180b210269c
Minh Ha <minhha1192003@gmail.com>
2025-08-19 23:42:47 +0700

    product-list

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/Admin/ProductController.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/Admin/ProductController.java
@@ -6,12 +6,13 @@ import com.vuminhha.decorstore.entity.Category;
 import com.vuminhha.decorstore.entity.Product;
 import com.vuminhha.decorstore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/products")
 public class ProductController {
     @Autowired
