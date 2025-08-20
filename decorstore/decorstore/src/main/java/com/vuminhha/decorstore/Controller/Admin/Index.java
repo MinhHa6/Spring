7eaf0a647afb003e29b30ca612bfa78fcbb9f61b
Minh Ha <minhha1192003@gmail.com>
2025-08-20 23:44:09 +0700

    Admin

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/Admin/Index.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/Admin/Index.java
@@ -1,7 +1,9 @@
 package com.vuminhha.decorstore.Controller.Admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +13,18 @@ public class Index {
     public String admin()
     {
         return "admin/index";
+    }
+
+    @GetMapping("/products")
+    public String product(Model model) {
+        // sau này bạn có thể lấy danh sách sản phẩm từ DB và add vào model
+        // model.addAttribute("products", productService.findAll());
+
+        return "admin/product-list"; // load admin/product.html
+    }
+    @GetMapping("/category")
+    public String category()
+    {
+        return "admin/category-list";
     }
 }
