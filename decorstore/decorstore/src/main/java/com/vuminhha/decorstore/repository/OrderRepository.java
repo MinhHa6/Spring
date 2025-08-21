b51fe82d44c66353e703e299018387f12501d5ab
Minh Ha <minhha1192003@gmail.com>
2025-08-21 23:51:33 +0700

    orderService

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/repository/OrderRepository.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.vuminhha.decorstore.repository;
 
 import com.vuminhha.decorstore.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OderRepository extends JpaRepository<Order,Long> {
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
 }
