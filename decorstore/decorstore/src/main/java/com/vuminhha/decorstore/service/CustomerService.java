98e7bdfeb40516025ea8437f72a0202f64bea68a
Minh Ha <minhha1192003@gmail.com>
2025-08-26 23:06:45 +0700

    customerservice:

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/service/CustomerService.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/service/CustomerService.java
@@ -1,7 +1,33 @@
 package com.vuminhha.decorstore.service;
 
+import com.vuminhha.decorstore.entity.Customer;
+import com.vuminhha.decorstore.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+    public List<Customer> getAll ()
+    {
+        return customerRepository.findAll();
+    }
+    // lay khach hang theo di a chi id
+    public Customer getCustomerById(Long id)
+    {
+        return customerRepository.findById(id).orElseThrow(()-> new RuntimeException("Customer null "));
+    }
+    // update -create
+    public Customer saveCustomer (Customer customer)
+    {
+        return customerRepository.save(customer);
+    }
+    // xoa khach hang theo id
+    public void deleteCustomer (Long id )
+    {
+        customerRepository.deleteById(id);
+    }
 }
