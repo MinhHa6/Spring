a399d847cc63f7b2c820a89868e967464084da40
Minh Ha <minhha1192003@gmail.com>
2025-10-14 00:00:05 +0700

    requestcontroller

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/Admin/CategoryController.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/Admin/CategoryController.java
@@ -7,47 +7,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/category")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
     @GetMapping
-    public String listCategories(Model model)
-    {
-        model.addAttribute("categories",
-        categoryService.getAll());
-        return "admin/category-list";
+    public List<Category> getAllCategories() {
+        return categoryService.getAll();
     }
-    @GetMapping("/create")
-    public String showCreateForm(Model model)
-    {
-        model.addAttribute("category",new Category());
-        return "admin/category-form";
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id) {
+        return categoryService.getCategoryId(id);
     }
-    @GetMapping("/edit/{id}")
-    public String showEditForm(@PathVariable ("id")Long id,Model model)
-    {
-        model.addAttribute("category",categoryService.getCategoryId(id));
-        return "admin/category-form";
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
     }
-    @PostMapping("/create")
-    public String saveCategory(@ModelAttribute ("category")Category category )
-    {
-        categoryService.saveCategory(category);
-        return "redirect:/category";
-    }
-    @PostMapping("/create/{id}")
-    public String updateCategory(@PathVariable Long id,@ModelAttribute Category category)
-    {
+
+    @PutMapping("/{id}")
+    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
         category.setId(id);
-        categoryService.saveCategory(category);
-        return "redirect:/category";
+        return categoryService.saveCategory(category);
     }
-    @GetMapping("/delete/{id}")
-    public String deleteCategory(@PathVariable Long id)
-    {
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
         categoryService.deleteCategory(id);
-        return "redirect:/category";
     }
 }
