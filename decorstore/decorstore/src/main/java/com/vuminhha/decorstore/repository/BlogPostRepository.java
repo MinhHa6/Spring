c36d766c2cade11f7bcb00ea05eb6ce8568bd8a4
Minh Ha <minhha1192003@gmail.com>
2025-10-15 01:16:45 +0700

    Blogpost

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/repository/BlogPostRepository.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/repository/BlogPostRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlogPostCategory extends JpaRepository<BlogPost,Long> {
+public interface BlogPostRepository extends JpaRepository<BlogPost,Long> {
 }
---
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/service/PostService.java
@@ -0,0 +1,38 @@
+package com.vuminhha.decorstore.service;
+
+import com.vuminhha.decorstore.entity.BlogPost;
+import com.vuminhha.decorstore.repository.BlogPostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final BlogPostRepository blogPostRepository;
+    public  PostService(BlogPostRepository blogPostRepository)
+    {
+        this.blogPostRepository=blogPostRepository;
+    }
+    // lay tat ca blog
+    public List<BlogPost> getAll()
+    {
+        return blogPostRepository.findAll();
+    }
+    // lay blog theo dia chi Id
+    public BlogPost getBlogById(Long id)
+    {
+        return blogPostRepository.findById(id).orElseThrow(()-> new RuntimeException("Blog not found with by id:"+id));
+    }
+    // them hoac cap nha blog
+    public BlogPost saveBlog(BlogPost blogPost)
+    {
+        return blogPostRepository.save(blogPost);
+    }
+    // xoa blog theo Id
+    public void deleteBlogById(Long id)
+    {
+        blogPostRepository.deleteById(id);
+    }
+}
