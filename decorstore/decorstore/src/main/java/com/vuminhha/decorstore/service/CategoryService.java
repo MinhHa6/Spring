f85fc6b79637232877b8f5760f9b3a677c5731a7
Minh Ha <minhha1192003@gmail.com>
2025-10-14 22:34:31 +0700

    categoryService

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/service/CategoryService.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/service/CategoryService.java
@@ -2,35 +2,39 @@ package com.vuminhha.decorstore.service;
 
 import com.vuminhha.decorstore.entity.Category;
 import com.vuminhha.decorstore.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
-    @Autowired
-    private CategoryRepository categoryRepository;
-    // lay toan bo category
-     public List<Category> getAll()
-     {
-         System.out.println(categoryRepository.findAll());
-         return categoryRepository.findAll();
-     }
-     //lay category theo id
-    public Category getCategoryId(Long id)
+
+    private final CategoryRepository categoryRepository;
+
+    //  inject CategoryRepository
+    public CategoryService (CategoryRepository categoryRepository)
     {
-        return categoryRepository.findById(id).orElseThrow(()->new RuntimeException("not found"));
+         this.categoryRepository=categoryRepository;
     }
-    // Update du lieu theo bang
-    public Category saveCategory( Category category)
-    {
+    public List<Category> getAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryId(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("not found"));
+    }
+
+    public Category saveCategory(Category category) {
         return categoryRepository.save(category);
     }
-    //xoa Category
-    public void deleteCategory( Long id )
-    {
+
+    public void deleteCategory(Long id) {
         categoryRepository.deleteById(id);
     }
 }
