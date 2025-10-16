fabace9317f0f595ccf6c865137a99842a2082e1
Minh Ha <minhha1192003@gmail.com>
2025-10-16 23:57:58 +0700

    PermissionService

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/entity/Permission.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/entity/Permission.java
@@ -17,4 +17,12 @@ public class Permission {
 
     @Column(length = 100, unique = true, nullable = false)
     private String name; // ex: "PRODUCT_READ", "PRODUCT_CREATE", "ORDER_MANAGE"
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/service/PermissionService.java
@@ -0,0 +1,50 @@
+package com.vuminhha.decorstore.service;
+
+import com.vuminhha.decorstore.entity.Permission;
+import com.vuminhha.decorstore.repository.PermissionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PermissionService {
+    private final PermissionRepository permissionRepository;
+    public PermissionService(PermissionRepository permissionRepository)
+    {
+        this.permissionRepository=permissionRepository;
+    }
+    // Lay tat ca quyen
+    public List<Permission> getAll()
+    {
+        return permissionRepository.findAll();
+    }
+    // Tao quyen moi
+    public Permission createPermission (String name)
+    {
+        if(permissionRepository.findByName(name).isPresent())
+        {
+            throw  new RuntimeException("Permission already exists:"+name);
+        }
+        Permission permission=new Permission();
+        permission.setName(name);
+        return permissionRepository.save(permission);
+    }
+    // tim quyen theo ten
+    public Optional<Permission> getPermissionByName(String name)
+    {
+        return permissionRepository.findByName(name);
+    }
+    // xoa quyen
+    public void deletePermission(Long id)
+    {
+        if(!permissionRepository.existsById(id))
+        {
+            throw  new RuntimeException("Permission not found with by id:"+id);
+        }
+        permissionRepository.deleteById(id);
+    }
+
+}
