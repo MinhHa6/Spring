aa7bbc3f936f077a95ef7e1d4fa4af65fec83904
Minh Ha <minhha1192003@gmail.com>
2025-10-30 00:57:07 +0700

    product_detail

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/user/ShopController.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/user/ShopController.java
@@ -1,14 +1,48 @@
 package com.vuminhha.decorstore.Controller.user;
 
+import com.vuminhha.decorstore.entity.Category;
+import com.vuminhha.decorstore.entity.Product;
+import com.vuminhha.decorstore.service.CategoryService;
+import com.vuminhha.decorstore.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 @Controller
 @RequestMapping("/shop")
 public class ShopController {
-    @GetMapping
-    public String shop()
+    private final CategoryService categoryService;
+    private final ProductService productService;
+    public ShopController (ProductService productService,CategoryService categoryService)
     {
-        return "users/shop";
+        this.productService=productService;
+        this.categoryService=categoryService;
     }
+    @GetMapping
+    public String viewShopPage(@RequestParam(value = "keyword", required = false) String keyword,
+                               @RequestParam(value = "categoryId", required = false) Long categoryId,
+                               Model model) {
+
+        List<Product> products;
+        List<Category> categories = categoryService.getAll();
+
+        if (keyword != null && !keyword.isEmpty()) {
+            products = productService.searchByName(keyword);
+            model.addAttribute("searchKeyword", keyword);
+        } else if (categoryId != null) {
+            products = productService.findByCategory(categoryId);
+            model.addAttribute("selectedCategoryId", categoryId);
+        } else {
+            products = productService.getAll();
+        }
+
+        model.addAttribute("categories", categories);
+        model.addAttribute("products", products);
+        return "users/shop"; // Giao diện shop.html (Decor Store)
+    }
+
 }
