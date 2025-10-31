1f9d2cd88e7e899f8383c426759da33cf50eadba
Minh Ha <minhha1192003@gmail.com>
2025-10-31 23:22:08 +0700

    .

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/user/CartController.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/Controller/user/CartController.java
@@ -1,4 +1,9 @@
 package com.vuminhha.decorstore.Controller.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cart")
 public class CartController {
 }
