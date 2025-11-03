2dbf56f970bd9ccddb530336ba37291c3d29b58e
Minh Ha <minhha1192003@gmail.com>
2025-11-03 23:52:32 +0700

    .

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/security/SecurityConfig.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/security/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 .formLogin(form -> form
                         .loginPage("/login")
                         .defaultSuccessUrl("/home", true)
+                        .defaultSuccessUrl("/admin",true)
                         .permitAll()
                 )
                 .logout(logout -> logout
