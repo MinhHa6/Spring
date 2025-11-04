9a6b21ec37a6c1b761629c25cadfcadad3f974fc
Minh Ha <minhha1192003@gmail.com>
2025-11-04 23:35:50 +0700

    orderController

--- decorstore/decorstore/src/main/java/com/vuminhha/decorstore/service/CustomerUserDetailsService.java
+++ decorstore/decorstore/src/main/java/com/vuminhha/decorstore/service/CustomerUserDetailsService.java
@@ -1,15 +1,15 @@
 package com.vuminhha.decorstore.service;
-
+import com.vuminhha.decorstore.entity.User;
 import com.vuminhha.decorstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,17 +19,18 @@ public class CustomerUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user=userRepository.findByUsername(username)
-                .orElseThrow(()-> new UsernameNotFoundException("User not found:"+username));
+        User user= userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("USer not found:"+username));
         if (!user.getActive()) {
             throw new UsernameNotFoundException("User is inactive: " + username);
         }
 
         // Convert roles to authorities
-        Set<GrantedAuthority> authorities = user.ge.stream()
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
                 .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toSet());
 
+        // tra ve spring security  userDetails
         return org.springframework.security.core.userdetails.User.builder()
                 .username(user.getUsername())
                 .password(user.getPassword())
